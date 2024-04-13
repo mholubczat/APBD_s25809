19f52d3549af498d78669408c5fde261f20c2e22
Maciej Hołubczat <m.holubczat@gmail.com>
2024-04-13 22:21:35 +0200

    Add userService test

---
+++ refactoring-example/LegacyAppTest/UserServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using LegacyApp;
+
+namespace LegacyAppTest;
+
+public class UserServiceTests
+{
+    private readonly UserService _userService = new();
+
+    [Test]
+    [TestCase("", "Rambo")]
+    [TestCase(null, "Rambo")]
+    [TestCase("John", "")]
+    [TestCase("John", null)]
+    public void AddUser_EmptyName_ShouldReturnFalse(string? firstName, string? lastName)
+    {
+        // Arrange
+        var dateOfBirth = new DateTime(1980, 1, 1);
+        const string email = "john.rambo@gmail.com";
+        const int clientId = 1;
+        
+        // Act
+        var isUserAdded = _userService.AddUser(firstName, lastName, email, dateOfBirth, clientId);
+        
+        // Assert
+        Assert.That(isUserAdded, Is.False);
+    }
+}
