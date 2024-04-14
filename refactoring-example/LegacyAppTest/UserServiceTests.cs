13df1b0bdbff4268135b762a639cd3c3099652fd
Maciej Hołubczat <m.holubczat@gmail.com>
2024-04-14 10:57:19 +0200

    Mock services, add tests

--- refactoring-example/LegacyAppTest/UserServiceTests.cs
+++ refactoring-example/LegacyAppTest/UserServiceTests.cs
@@ -1,27 +1,90 @@
 ﻿using LegacyApp;
+using Moq;
 
 namespace LegacyAppTest;
 
 public class UserServiceTests
 {
-    private readonly UserService _userService = new();
+    private UserService _userService;
+    private Mock<IClientRepository> _clientRepositoryMock;
+    private Mock<IUserCreditService> _userCreditServiceMock;
 
-    [Test]
-    [TestCase("", "Rambo")]
-    [TestCase(null, "Rambo")]
-    [TestCase("John", "")]
-    [TestCase("John", null)]
-    public void AddUser_EmptyName_ShouldReturnFalse(string? firstName, string? lastName)
+    private void SetupMocksToReturnClientWithCredit(Client client, int credit)
     {
-        // Arrange
+        _clientRepositoryMock = new Mock<IClientRepository>();
+        _clientRepositoryMock
+            .Setup(x => x.GetById(It.IsAny<int>()))
+            .Returns(client);
+
+        _userCreditServiceMock = new Mock<IUserCreditService>();
+        _userCreditServiceMock
+            .Setup(x => x.GetCreditLimit(It.IsAny<User>()))
+            .Returns(credit);
+        
+        _userService = new UserService(
+            _clientRepositoryMock.Object,
+            _userCreditServiceMock.Object,
+            new UserValidator());
+    }
+    
+    private readonly Client _johnRambo = new()
+    {
+        Name = "John Rambo",
+        ClientId = 1,
+        Email = "john.rambo@gmail.com",
+        Address = "Jungle",
+        Type = ClientType.NormalClient
+    };
+    
+    [Test]
+    [TestCase("", "Rambo", false)]
+    [TestCase(null, "Rambo", false)]
+    [TestCase("John", "", false)]
+    [TestCase("John", null, false)]
+    [TestCase("John", "Rambo", true)]
+    public void AddUser_EmptyName_ShouldReturnFalse(string? firstName, string? lastName, bool result)
+    {
+        //Arrange
+        SetupMocksToReturnClientWithCredit(_johnRambo, 1000);
         var dateOfBirth = new DateTime(1980, 1, 1);
-        const string email = "john.rambo@gmail.com";
-        const int clientId = 1;
         
         // Act
-        var isUserAdded = _userService.AddUser(firstName, lastName, email, dateOfBirth, clientId);
+        var isUserAdded = _userService.AddUser(firstName, lastName, _johnRambo.Email, dateOfBirth, _johnRambo.ClientId);
         
         // Assert
-        Assert.That(isUserAdded, Is.False);
+        Assert.That(result, Is.EqualTo(isUserAdded));
+    }
+    
+    [Test]
+    [TestCase(1, false)]
+    [TestCase(-1, true)]
+    public void AddUser_Underage_ShouldReturnFalse(int monthsTo21Birthday, bool result)
+    {
+        //Arrange
+        SetupMocksToReturnClientWithCredit(_johnRambo, 1000);
+        var dateOfBirth = DateTime.Now.AddYears(-21).AddMonths(monthsTo21Birthday);
+        
+        // Act
+        var isUserAdded = _userService.AddUser("John", "Rambo", _johnRambo.Email, dateOfBirth, _johnRambo.ClientId);
+        
+        // Assert
+        Assert.That(result, Is.EqualTo(isUserAdded));
+    }
+    
+    [Test]
+    [TestCase("john.rambo.gmail.com", false)]
+    [TestCase("johnrambo@gmailcom", false)]
+    [TestCase("john.rambo@gmail.com", true)]
+    public void AddUser_InvalidEmail_ShouldReturnFalse(string email, bool result)
+    {
+        //Arrange
+        SetupMocksToReturnClientWithCredit(_johnRambo, 1000);
+        var dateOfBirth = new DateTime(1980, 1, 1);
+        
+        // Act
+        var isUserAdded = _userService.AddUser("John", "Rambo", email, dateOfBirth, _johnRambo.ClientId);
+        
+        // Assert
+        Assert.That(result, Is.EqualTo(isUserAdded));
     }
 }
