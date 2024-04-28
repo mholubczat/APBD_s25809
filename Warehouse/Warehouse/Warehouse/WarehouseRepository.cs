7ad8d791e69ccd43c9e6e7f1ddeebefc6b756850
Maciej Hołubczat <s25809@pjwstk.edu.pl>
2024-04-28 17:22:52 +0200

    Prepare add product query

--- Warehouse/Warehouse/Warehouse/WarehouseRepository.cs
+++ Warehouse/Warehouse/Warehouse/WarehouseRepository.cs
@@ -1,4 +1,5 @@
 using System.Data.SqlClient;
+using Microsoft.Extensions.Options;
 
 namespace Warehouse;
 
@@ -22,6 +23,10 @@ public class WarehouseRepository : IWarehouseRepository
         using var connection = new SqlConnection(_configuration["ConnectionStrings:DefaultConnection"]);
         connection.Open();
         using var command = new SqlCommand();
+
+        var fileInfo = new FileInfo("create.sql");
+        command.CommandText = "IF NOT EXISTS SELECT 1 FROM sys.tables WHERE Name = 'Warehouse' BEGIN " +
+                              fileInfo.OpenText().ReadToEnd() + " END";
         command.Connection = connection;
 
         command.ExecuteNonQuery();
@@ -29,6 +34,34 @@ public class WarehouseRepository : IWarehouseRepository
 
     public async Task<int> AddProduct(AddProductModel model)
     {
-        throw new NotImplementedException();
+        if (Validate(model) == false)
+        {
+            return 0;
+        }
+
+        await using var connection = new SqlConnection(_configuration["ConnectionStrings:DefaultConnection"]);
+        await connection.OpenAsync();
+
+        await using var command = new SqlCommand();
+        command.CommandText = "IF EXISTS SELECT 1 FROM Product WHERE IdProduct = @ProductId " +
+                              "AND EXISTS SELECT 1 FROM Warehouse WHERE IdWarehouse = @WarehouseId " +
+                              "AND EXISTS SELECT 1 FROM Order o " +
+                                  "LEFT JOIN Product_Warehouse pw ON o.OrderId = pw.OrderId " +
+                                  "WHERE o.IdProduct = @ProductId AND o.Amount = @Amount AND o.CreatedAt < @CreatedAt AND pw.OrderId IS NULL " +
+                              "BEGIN " +
+                                  "DECLARE @OrderId INT = SELECT OrderId FROM ORDER WHERE o.IdProduct = @ProductId AND o.Amount = @Amount AND o.CreatedAt < @CreatedAt" +  
+                                  "DECLARE @OrderId INT = SELECT OrderId FROM ORDER WHERE o.IdProduct = @ProductId AND o.Amount = @Amount AND o.CreatedAt < @CreatedAt" + 
+                                  "BEGIN TRANSACTION " +
+                                      "UPDATE ORDER SET FullfilledAt = GETDATE() " +
+                                      "INSERT INTO Product_Warehouse (IdWarehouse, IdProduct, IdOrder, Amount, Price, CreatedAt) " +
+                                          "SELECT @WarehouseId, @ProductId, @OrderId, @Amount, @Amount * p.Price, GETDATE()" +
+                                              "FROM Product p WHERE p.IdProduct = @ProductId";
+
+        return 1;
+    }
+
+    private static bool Validate(AddProductModel model)
+    {
+        return model is { ProductId: >= 1, WarehouseId: >= 1, Amount: >= 1 };
     }
 }
